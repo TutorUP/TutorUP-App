4036a66e3c8f288ad8099325515facd1f03a2236
Danh Nguyen <danhnguyen267@gmail.com>
2018-12-16 01:19:01 -0800

    Added navbar and login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// components
+import AppNavbar from './components/layout/AppNavbar';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CssBaseline />
+        <AppNavbar />
+        <Login />
+     
+
       </div>
     );
   }
