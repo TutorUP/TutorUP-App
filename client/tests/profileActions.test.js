46cd376beb8bf0bbc1d9273a89cbab7b15d6940e
Danh Nguyen <danhnguyen267@gmail.com>
2019-01-13 02:30:28 -0800

    Introduce client app testing with Jest

---
+++ client/tests/profileActions.test.js
@@ -0,0 +1,31 @@
+import * as actions from '../src/redux/actions/profileActions';
+import * as types from '../src/redux/actions/types';
+
+describe('Get current user profile', () => {
+    it('should create an action to get a profile', () => {
+        const text = 'Profile';
+        const expectedAction = {
+            type: types.GET_PROFILE,
+            text
+        }
+        expect(actions.getCurrentProfile(text)).toEqual(expectedAction);
+    });
+});
+
+describe('Clear current profile', () => {
+    it('should create an action to clear a profile', () => {
+        const expectedAction = {
+            type: types.CLEAR_CURRENT_PROFILE
+        }
+        expect(actions.clearCurrentProfile()).toEqual(expectedAction);
+    });
+});
+
+describe('Profile loading', () => {
+    it('should create an action to indicate a loading profile', () => {
+        const expectedAction = {
+            type: types.PROFILE_LOADING
+        }
+        expect(actions.setProfileLoading()).toEqual(expectedAction);
+    });
+});
