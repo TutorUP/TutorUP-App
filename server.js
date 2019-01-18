0822b684785fdb0e24a0dba304b5c56a6eabb4ae
Danh Nguyen <danhnguyen267@gmail.com>
2019-01-18 03:23:28 -0800

    Changed typo

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
     sessConfig.cookie.secure = true;
 
     // Set static folder
-    app.use(expess.static('client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
