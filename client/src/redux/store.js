79a98f2ba57b2b877fc66760f2f46fb1f490bdeb
Danh Nguyen <danhnguyen267@gmail.com>
2019-02-01 19:39:18 -0800

    App works on browsers without redux devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -6,13 +7,12 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const composedEnhancer = composeWithDevTools(applyMiddleware(...middleware))
+
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composedEnhancer
 );
 
 export default store;
