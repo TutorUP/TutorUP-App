9ac3c475fc06bff2049dd3d331333d6bf8e8eb2c
Danh Nguyen <danhnguyen267@gmail.com>
2019-02-11 20:54:44 -0800

    Adding mailto links for profiles

--- client/src/components/showcase/ProfileItem.js
+++ client/src/components/showcase/ProfileItem.js
@@ -74,7 +74,8 @@ const ProfileItem = props => {
                             <MailIcon className="icon"/>
                         </Grid>
                         <Grid item xs>
-                            <Typography>{profile.user.email}</Typography>
+
+                            <Typography><a style={{ color: '#838383', textDecorationLine: 'none'}} href={`mailto:${profile.user.email}`}>{profile.user.email}</a></Typography>
                         </Grid>
                     </Grid>
                     <Grid container wrap="nowrap" spacing={16}>
