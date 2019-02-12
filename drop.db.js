6d05b8cc4ae6be0179224dafcd26ce1fc6792075
Danh Nguyen <danhnguyen267@gmail.com>
2019-02-11 23:31:17 -0800

    Added script to delete db

---
+++ drop.db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, () => {
+  mongoose.connection.db.dropDatabase()
+    .then(() => process.exit())
+});
+
+console.log('TutorUP database dropped');
