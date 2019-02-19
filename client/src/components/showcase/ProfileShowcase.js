1bafdb424e408787a7daccfd1e7f2e3ed3452f5b
Elias Paraiso <paraiso18@up.edu>
2019-02-19 10:23:19 -0800

    Updated Search Bar and Filter Dropdowns

--- client/src/components/showcase/ProfileShowcase.js
+++ client/src/components/showcase/ProfileShowcase.js
@@ -20,7 +20,7 @@ import { getSubjects } from '../../redux/actions/subjectActions';
 
 // MUI imports
 import { withStyles } from '@material-ui/core/styles';
-import { FormControl, Input, InputLabel } from '@material-ui/core';
+import { Input } from '@material-ui/core';
 
 const styles = {
     card: {
@@ -40,12 +40,13 @@ class ProfilesShowcase extends Component {
         this.state = {
         data: getProfiles(),
         searching: false,
+        filtering: false,
         searchText: '',
         text: '',
         orderDropDown: null,
         filterDropDown: null,
         majorsDropDown: null,
-        subjects: [],
+        subjects: getSubjects(),
         major: [],
         shuffled: false,
       };
@@ -54,11 +55,12 @@ class ProfilesShowcase extends Component {
     //Functions then render
     componentDidMount() {
         this.props.getProfiles();
+        this.props.getSubjects();
     }
 
     //for randomizing profiles displayed
     shuffle = event => {
-        if(this.state.searching == true){
+        if(this.state.searching === true){
             var array = this.state.data;
         }
         else{
@@ -96,107 +98,6 @@ class ProfilesShowcase extends Component {
         this.setState({
             majorsDropDown: null,
         })
-    }
-
-    //uses current value of search bar to find tutors based on name, major, or the courses they tutor   
-    handleSearch = text => event => {   
-        this.setState({
-            [text]: event.target.searchText
-        });
-        let search_text = event.target.value;
-        let profileList = this.props.profile.profiles;
-        console.log("Search Start!");
-        if (search_text.length > 0){
-           
-            let searchList = [];  //searchList will contain all the matching profiles from the search_text input
-            for(var prof in profileList){
-                let profile = profileList[prof];
-                
-                //find matching course attributes
-                let courses = profile.courses;
-                if(courses.length > 0){
-                    for(var c in courses){
-                        let course = courses[c];
-                        //Course Number ex: 301, 303, ...
-                        if(course.courseNumber == search_text){
-                            if(searchList.includes(profile)){
-                                console.log("profile already in list: " + firstName);
-                            }
-                            else{
-                                console.log(profile.user.firstname + " added by course #" + course.courseNumber);
-                                searchList.push(profile);
-                            }
-                        }
-                       //Course ID ex: CS, HST, MTH...
-                        if(course.courseId.toLowerCase() == search_text.toLowerCase()){
-                            if(searchList.includes(profile)){
-                                console.log("profile already in list: " + firstName);
-                            }
-                            else{
-                                console.log(profile.user.firstname  + " added by courseId " + course.courseId);
-                                searchList.push(profile);
-                            }
-                        }
-                        //combo of the two above... ex: CS301 or CS 301
-                        if(search_text.toLowerCase() == (course.courseId + course.courseNumber).toLowerCase() 
-                            || search_text.toLowerCase() == (course.courseId + ' ' + course.courseNumber).toLowerCase()){
-                                if(searchList.includes(profile)){
-                                    console.log("profile already in list: " + firstName);
-                                }
-                                else{
-                                    console.log(profile.user.firstname  + " added by specific course " + course.courseId + ' ' + course.courseNumber);
-                                    searchList.push(profile);
-                                }
-                        }
-                    }
-                }
-
-                //compare first name to search text, add to list if similar
-                let firstName = profile.user.firstname;
-                if (firstName.length >= search_text.length){
-                    let userName = firstName.substring(0, search_text.length).toLowerCase();
-                    if(userName.includes(search_text.toLowerCase())){
-                        //check to see if profile is has already been hit
-                        if(searchList.includes(profile)){
-                            console.log("profile already in list: " + firstName);
-                        }
-                        else{
-                            searchList.push(profile);
-                            console.log(firstName + " added by name");
-                        }  
-                    }
-                }
-                // by major
-                let majors = profile.major;
-                for(var m in majors){
-                    let major = majors[m];
-                    if(major.length >= search_text.length){
-                        let majorSub = major.substring(0, search_text.length).toLowerCase();
-                        if(search_text.toLowerCase() === majorSub){
-                            if(searchList.includes(profile)){
-                                console.log("profile already in list: " + firstName);
-                            }
-                            else{
-                                console.log(firstName + " added by major");
-                                searchList.push(profile);
-                            }
-                        }
-                    }
-                }
-            }
-            //updates displayed profiles with search list 
-            if(searchList.length > 0){
-                this.setState(state => ({
-                    data: searchList,
-                    searching: true
-                }));
-            }
-        }
-        else { //if there is no text in search bar, don't use search list
-            this.setState(state => ({
-                searching: false
-            }));
-        }
     }
 
     handleFilterBy = event => {
@@ -243,7 +144,7 @@ class ProfilesShowcase extends Component {
 
     // sort currently displayed profiles by name alphabetically 
     orderByName = (order) => event => {
-        if(this.state.searching == false){
+        if(this.state.searching === false){
             var profileList = this.props.profile.profiles;
             let nameSorted = profileList.sort(function(a,b) {
                 var nameA=a.user.firstname.toLowerCase();
@@ -275,46 +176,153 @@ class ProfilesShowcase extends Component {
         }
     }
 
-    //TODO
-    filterBy = (filterBy) => {
+     //uses current value of search bar to find tutors based on name, major, or the courses they tutor   
+     handleSearch = text => event => {   
+        this.setState({
+            [text]: event.target.searchText
+        });
+        let search_text = event.target.value;
         let profileList = this.props.profile.profiles;
+        console.log("Search Start!");
+        if (search_text.length > 0){
+            let searchList = [];  //searchList will contain all the matching profiles from the search_text input
+            for(var prof in profileList){
+                let profile = profileList[prof];
+                let firstName = profile.user.firstname;
+                //find matching course attributes
+                let courses = profile.courses;
+                if(courses.length > 0){
+                    for(var c in courses){
+                        let course = courses[c];
+                        //Course Number ex: 301, 303, ...
+                        if(course.courseNumber === search_text){
+                            if(searchList.includes(profile)){
+                                console.log("profile already in list: " + firstName);
+                            }
+                            else{
+                                console.log(profile.user.firstname + " added by course #" + course.courseNumber);
+                                searchList.push(profile);
+                            }
+                        }
+                       //Course ID ex: CS, HST, MTH...
+                        if(course.courseId.toLowerCase() === search_text.toLowerCase()){
+                            if(searchList.includes(profile)){
+                                console.log("profile already in list: " + firstName);
+                            }
+                            else{
+                                console.log(profile.user.firstname  + " added by courseId " + course.courseId);
+                                searchList.push(profile);
+                            }
+                        }
+                        //combo of the two above... ex: CS301 or CS 301
+                        if(search_text.toLowerCase() === (course.courseId + course.courseNumber).toLowerCase() 
+                            || search_text.toLowerCase() === (course.courseId + ' ' + course.courseNumber).toLowerCase()){
+                                if(searchList.includes(profile)){
+                                    console.log("profile already in list: " + firstName);
+                                }
+                                else{
+                                    console.log(profile.user.firstname  + " added by specific course " + course.courseId + ' ' + course.courseNumber);
+                                    searchList.push(profile);
+                                }
+                        }
+                    }
+                }
 
-        var i;
-        for(i = 0; i<profileList.length; i++){
-            let prof = profileList[i];
-            console.log(prof.major);
-            console.log(prof.courses);
-            console.log(filterBy);
-        }   
-    }
-
-    //TODO
-    filterByMajor = (filterBy) => {
-        console.log("TO DO Filter by Major ...")
-
-    }
-    //TODO
-    createMajorsDropDown = event => {
-        let profileList = this.profile.profiles;
-        let courseList = [];
-        for(var prof in profileList){
-            let  profile = profileList[prof];
-            if(profile.courses != null){
-                console.log(profile.courses);
+                //compare first name to search text, add to list if similar
+                if (firstName.length >= search_text.length){
+                    let userName = firstName.substring(0, search_text.length).toLowerCase();
+                    if(userName.includes(search_text.toLowerCase())){
+                        //check to see if profile is has already been hit
+                        if(searchList.includes(profile)){
+                            console.log("profile already in list: " + firstName);
+                        }
+                        else{
+                            searchList.push(profile);
+                            console.log(firstName + " added by name");
+                        }  
+                    }
+                }
+                // by major
+                let majors = profile.major;
+                for(var m in majors){
+                    let major = majors[m];
+                    if(major.length >= search_text.length){
+                        let majorSub = major.substring(0, search_text.length).toLowerCase();
+                        if(search_text.toLowerCase() === majorSub){
+                            if(searchList.includes(profile)){
+                                console.log("profile already in list: " + firstName);
+                            }
+                            else{
+                                console.log(firstName + " added by major");
+                                searchList.push(profile);
+                            }
+                        }
+                    }
+                }
+            }
+            //updates displayed profiles with search list 
+            if(searchList.length > 0){
+                this.setState(state => ({
+                    data: searchList,
+                    searching: true
+                }));
             }
         }
+        else { //if there is no text in search bar, don't use search list
+            this.setState(state => ({
+                searching: false
+            }));
+        }
+    }
+
+    //Returns results from profiles relating to chosen subject
+    filterBySubject = (filterBy) => event => {
+        let profileList = this.props.profile.profiles;
+        let results = [];
+        for(var prof in profileList){
+            let profile = profileList[prof];
+            let majors = profile.major;
+            for(var m in majors){
+                let major = majors[m];
+                if(filterBy === major && !results.includes(profile)){
+                    results.push(profile);
+                }
+            }
+        }
+        this.setState({
+            data: results,
+            filtering: true,
+        });
+    }
+
+    filterByPaid = (paid) => event => {
+        let profileList = this.props.profile.profiles;
+        let results = [];
+        for(var prof in profileList){
+            let profile = profileList[prof];
+            let type = profile.type;
+            if(type === "Paid" && !results.includes(profile)){
+                results.push(profile);
+            }
+        }
+        this.setState({
+            data: results,
+            filtering: true,
+        });
     }
 
     render() {
         const { classes } = this.props;
         const { profiles, loading } = this.props.profile;
-        const { orderDropDown, filterDropDown, majorsDropDown, subjects, major} = this.state;
-        
-        const majors = _.filter(subjects, ['isMajor', "Yes"]);
+        const { orderDropDown, filterDropDown, majorsDropDown, subjects} = this.state;
 
-        const majorMenuItems =  majors.map((major, i) =>
-                <MenuItem key={i} value={major.name}>{major.name}</MenuItem>
-        );
+        let allSubjects = this.props.subjects.subjects;  
+        let courseMenuItems = (<MenuItem value="None">None</MenuItem>);
+        if(allSubjects){
+            courseMenuItems = allSubjects.map((subject, i) =>
+                <MenuItem key={i} value={subject.name} onClick={this.filterBySubject(subject.name)}>{subject.name}</MenuItem>
+            );
+        }
 
         let profileItems;
 
@@ -322,7 +330,7 @@ class ProfilesShowcase extends Component {
             profileItems = (<ProgressSpinner />)
         }
         else {  //if text in search bar, display search results
-            if(this.state.searching === true){
+            if(this.state.searching === true || this.state.filtering === true || this.state.shuffled == true){
                 let searchData = this.state.data;
                 profileItems = searchData.length > 0 ?
                 searchData.map(profile => (
@@ -333,29 +341,16 @@ class ProfilesShowcase extends Component {
                     </div>
                 );
             }
-            else {  
-                if(this.state.shuffled === true){ //shuffle of all profiles
-                    let shuffledData = this.state.data;
-                    profileItems = shuffledData.length > 0 ?
-                    shuffledData.map(profile => (
-                        <ProfileItem key={profile._id} profile={profile} />
-                    )) : (
-                        <div>
-                            <h1>No Profiles To List</h1>
-                        </div>
-                    );
-                }
-                else{
-                    let Profiles = profiles;
-                    profileItems = profiles.length > 0 ?
-                    Profiles.map(profile => (
-                        <ProfileItem key={profile._id} profile={profile} />
-                    )) : (
-                        <div>
-                            <h1>No Profiles To List</h1>
-                        </div>
-                    );
-                }
+            else { 
+                let Profiles = profiles;
+                profileItems = profiles.length > 0 ?
+                Profiles.map(profile => (
+                    <ProfileItem key={profile._id} profile={profile} />
+                )) : (
+                    <div>
+                        <h1>No Profiles To List</h1>
+                    </div>
+                );
             }
         }
         return (
@@ -376,17 +371,14 @@ class ProfilesShowcase extends Component {
 
                                             <Button variant="contained" className={classes.marginLeft20} aria-haspopup="true" onClick={this.handleFilterBy} aria-owns={filterDropDown ? 'filterByMenu' : undefined}>Filter by</Button>
                                             <Menu id='filterByMenu' anchorEl={filterDropDown} open={Boolean(filterDropDown)} onClose={this.closeFilterMenu}>
-                                                <MenuItem onClick={() => this.filterBy('name')}> First Name</MenuItem>
-                                                <MenuItem onClick={() => this.filterBy('class')}> Course Name</MenuItem>
-
+                                                <MenuItem onClick={this.filterByPaid('paid')}> Paid or Volunteer</MenuItem>
+                                                <MenuItem onClick={this.filterByPaid('class')}> Course Name</MenuItem>
                                                 <Button className={classes.marginLeft20} aria-haspopup="true" onClick={this.handleMajorsMenu} aria-owns={majorsDropDown ? 'majorsMenu' : undefined}>Majors</Button>
-                                                <Menu id="majorsMenu" anchorEl={majorsDropDown} open={Boolean(majorsDropDown)} onClose={this.closeMajorsMenu}>
-                                                    {majorMenuItems}
-                                                </Menu>
-                                            </Menu>
-                                            
+                                                    <Menu id="majorsMenu" anchorEl={majorsDropDown} open={Boolean(majorsDropDown)} onClose={this.closeMajorsMenu}>
+                                                        {courseMenuItems}
+                                                    </Menu>
+                                            </Menu> 
                                             <Button variant="contained" className={classes.marginLeft20} onClick={this.shuffle}>Shuffle</Button>
-
                                             <br/>
                                         </React.Fragment>
                             </Paper>
@@ -405,11 +397,13 @@ class ProfilesShowcase extends Component {
 }
 
 ProfilesShowcase.propTypes = {
-    profile: PropTypes.object.isRequired
+    profile: PropTypes.object.isRequired,
+    subjects: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state => ({
-    profile: state.profile
+    profile: state.profile,
+    subjects: state.subjects,
 });
 
-export default connect(mapStateToProps, { getProfiles })(withStyles(styles)(ProfilesShowcase))
+export default connect(mapStateToProps, { getProfiles, getSubjects })(withStyles(styles)(ProfilesShowcase))
