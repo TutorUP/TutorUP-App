993def92f97ca7713ae8d3a14e08da6b3effb02a
baldwina18 <baldwina18@up.edu>
2019-03-01 15:05:47 -0800

    Spacing fix

--- client/src/components/admin/EditSubjects.js
+++ client/src/components/admin/EditSubjects.js
@@ -155,8 +155,8 @@ render() {
             <Typography variant="h4" component="h1" align="center" className="editHeading">
                 Create and Edit Subjects
             </Typography>
-            {subjectItems}
             <Grid container justify="space-between" spacing={24}>
+              {subjectItems}
                <Grid item xs={12}>
                    <div className="courses"></div>
                </Grid>
