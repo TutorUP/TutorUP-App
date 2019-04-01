f52aa005258ebea2643f91cb329bf408f4a73153
baldwina18 <baldwina18@up.edu>
2019-03-31 23:36:12 -0700

    About ip

--- client/src/components/common/About.js
+++ client/src/components/common/About.js
@@ -8,6 +8,7 @@ import Divider from '@material-ui/core/Divider';
 import Img1 from '../../images/about-1.png';
 import Img2 from '../../images/about-2.png';
 import Img3 from '../../images/about-3.png';
+import '../profile/profile.css';
 
 
 import { withStyles } from '@material-ui/core/styles';
@@ -26,74 +27,39 @@ class About extends Component {
         const { classes } = this.props;
       
         return (
-            <div className="padding20 about-img">
-              <Grid container spacing={24}>
+            <div className="padding20">
                 <Grid item xs={12}>
-                  <Typography variant="h3" align="center">
+                  <Typography variant="h5" align="center">
                     Tutor UP is a peer tutoring network for University of Portland.
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
                   <Divider variant="middle" className="m15"/>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                  <Typography variant="h2" align="left" gutterBottom>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="h5"  gutterBottom>
                     Accessibility.    
                   </Typography>
-                  <Typography variant="subtitle1" align="left">
+                  <Typography variant="subtitle1" gutterBottom>
                     Tutor UP was created with accessibility in mind. Our goal is to make academic aid more accessible for students.
                   </Typography>
                 </Grid>
-                <Grid item xs={12} md={6} className="box">
-                  <Card className="img-card">
-                      <CardMedia
-                        component="img"
-                        alt="edit profile"
-                        className={classes.media}
-                        height="200"
-                        image={Img1}
-                      />
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={6} className="box">
-                  <Card className="img-card">
-                      <CardMedia
-                        component="img"
-                        alt="edit profile"
-                        className={classes.media}
-                        height="200"
-                        image={Img2}
-                      />
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <Typography variant="h2" align="right" gutterBottom>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="h5" gutterBottom>
                     Simplicity.
                   </Typography>
-                  <Typography variant="subtitle1" align="right">
+                  <Typography variant="subtitle1" gutterBottom>
                     No account is needed to find a tutor! Simply navigate to the search page to begin finding assistance!
                   </Typography>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                  <Typography variant="h2" align="left" gutterBottom>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="h5" gutterBottom>
                     Diversity.    
                   </Typography>
-                  <Typography variant="subtitle1" align="left">
+                  <Typography variant="subtitle1" gutterBottom>
                     This online platform allows students to find tutors in a more diverse range of subjects than is offered through on-campus programs.
                   </Typography>
                 </Grid>
-                <Grid item xs={12} md={6} className="box">
-                  <Card className="img-card">
-                      <CardMedia
-                        component="img"
-                        alt="edit profile"
-                        className={classes.media}
-                        height="200"
-                        image={Img3}
-                      />
-                    </Card>
-                </Grid>
-              </Grid>
             </div>
         );    
   }
